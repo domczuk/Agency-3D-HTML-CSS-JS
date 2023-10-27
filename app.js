2e8b1865c3b7e580b96862bc3013a84711d30380
domczuk <nowakd2115@gmail.com>
2023-10-28 01:15:55 +0200

    Project

---
+++ app.js
@@ -0,0 +1,10 @@
+// app.js
+function openSidebar() {
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeSidebar() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
